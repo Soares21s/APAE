253e3a61545475095ea5f8c669cd41b11fc87614
SoByte/ <soares21sassa@gmail.com>
2025-09-23 11:03:04 -0300

    routes

--- apae/routes.py
+++ apae/routes.py
@@ -4,3 +4,15 @@ from apae import app
 @app.route('/')
 def homepage():
     return render_template("homepage.html")
+
+@app.route('/historia')
+def story():
+    return render_template('story.html')
+
+@app.route('/clinica')
+def clinic():
+    return render_template('clinic.html')
+
+@app.route('/seja_voluntario')
+def voluntary():
+    return render_template('voluntary.html')
